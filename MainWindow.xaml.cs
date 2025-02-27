d7997c9a531a157a598b64a211553e1457288612
epic-unicorn <ingmar.struijs@pm.me>
2025-02-27 23:04:21 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using MediaDevices;
+
+namespace PhoneBackup
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReadFolders_Click(object sender, RoutedEventArgs e)
+        {
+            treeViewFolders.Items.Clear();
+            var devices = MediaDevice.GetDevices();
+            foreach (var device in devices)
+            {
+                device.Connect();
+                var rootItem = new TreeViewItem { Header = device.Description };
+                treeViewFolders.Items.Add(rootItem);
+                ReadFolders(device, rootItem, device.GetRootDirectory().FullName);
+                device.Disconnect();
+            }
+        }
+
+        private void ReadFolders(MediaDevice device, TreeViewItem parentItem, string path)
+        {
+            var directories = device.EnumerateDirectories(path);
+            foreach (var directory in directories)
+            {
+                var directoryItem = new TreeViewItem { Header = Path.GetFileName(directory) };
+                parentItem.Items.Add(directoryItem);
+                ReadFolders(device, directoryItem, directory);
+            }
+        }
+    }
+}
+
